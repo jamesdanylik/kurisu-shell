fd4219e9f304926fcc6093837c97210c4891da72
James Danylik <james.danylik@gmail.com>
2013-10-08 21:38:31 -0700

    Reads scripts into a dynamic buffer

--- read-command.c
+++ read-command.c
@@ -1,27 +1,59 @@
 // UCLA CS 111 Lab 1 command reading
 
+/* Skeleton Includes */
 #include "command.h"
 #include "command-internals.h"
 
 #include <error.h>
 
+/*My Includes */
+#include "alloc.h"
+#include <stdio.h>
+
 /* FIXME: You may need to add #include directives, macro definitions,
    static function definitions, etc.  */
 
-/* FIXME: Define the type 'struct command_stream' here.  This should
+/* INPROGRESS: Define the type 'struct command_stream' here.  This should
    complete the incomplete type declaration in command.h.  */
+
+struct command_stream
+{
+  int size;
+  command_t *commands;
+};
 
 command_stream_t
 make_command_stream (int (*get_next_byte) (void *),
 		     void *get_next_byte_argument)
 {
-  /* FIXME: Replace this with your implementation.  You may need to
+  /* INPROGRESS: Replace this with your implementation.  You may need to
      add auxiliary functions and otherwise modify the source code.
      You can also use external functions defined in the GNU C Library.  */
-  (void)get_next_byte;
-  (void)get_next_byte_argument;
 
-  error (1, 0, "command reading not yet implemented");
+  /* Begin by reading the shell script into a buffer for processing */
+
+  /* Some variables to hold the buffer and track its size */
+  size_t bufferSize = 256; 
+  size_t bytesRead = 0;    
+  int currentByte;  
+  char *buffer = (char*) checked_malloc(bufferSize);
+
+  /* Read the entire script into the buffer.  Double in size if the script
+     exceeds what we currently have allocated.  Terminate the buffer with a
+     null byte.  */
+  while ( (currentByte = get_next_byte(get_next_byte_argument)) != EOF )
+  {
+    buffer[bytesRead++] = currentByte;
+    if ( bytesRead == bufferSize)
+    {
+      buffer = (char*) checked_grow_alloc(buffer, &bufferSize);
+    } 
+  }
+  buffer[bytesRead] = 0;
+  // Proof this code works: 
+  // printf ("%s \n", buffer);
+
+  error (1, 0, "command reading not FULLY implemented");
   return 0;
 }
 
